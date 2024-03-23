3b1e2a8fd214e50b7786f525f69b79cca5b3fa45
rbalsleyMSFT <53497092+rbalsleyMSFT@users.noreply.github.com>
2024-03-22 17:42:45 -0700

    Update script version to 2403.1

--- FFUDevelopment/WinPEDeployFFUFiles/ApplyFFU.ps1
+++ FFUDevelopment/WinPEDeployFFUFiles/ApplyFFU.ps1
@@ -117,7 +117,7 @@ $LogFileName = 'ScriptLog.txt'
 $USBDrive = Get-USBDrive
 New-item -Path $USBDrive -Name $LogFileName -ItemType "file" -Force | Out-Null
 $LogFile = $USBDrive + $LogFilename
-$version = '2402.1'
+$version = '2403.1'
 WriteLog 'Begin Logging'
 WriteLog "Script version: $version"
 
