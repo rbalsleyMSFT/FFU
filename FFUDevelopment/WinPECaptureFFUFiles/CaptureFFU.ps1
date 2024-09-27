b3b6017e0b37863d29d1930cb398fa02a3c9cb33
JonasKloseBW <jonas.klose@iz.bwl.de>
2024-09-27 19:31:27 +0200

    Update CaptureFFU.ps1
    - Implement CustomFFUNameTemplate code to set the correct FFU output file name

--- FFUDevelopment/WinPECaptureFFUFiles/CaptureFFU.ps1
+++ FFUDevelopment/WinPECaptureFFUFiles/CaptureFFU.ps1
@@ -1,5 +1,6 @@
 #Modify the net use W: \\192.168.1.158\FFUCaptureShare /user:ffu_user ddb1f077-3eed-433c-b4d9-7b8cd54ce727
 net use W: \\192.168.1.158\FFUCaptureShare /user:ffu_user ddb1f077-3eed-433c-b4d9-7b8cd54ce727
+#Custom naming placeholder
 
 $AssignDriveLetter = 'x:\AssignDriveLetter.txt'
 Start-Process -FilePath diskpart.exe -ArgumentList "/S $AssignDriveLetter" -Wait -ErrorAction Stop | Out-Null
@@ -40,16 +41,31 @@ else{
 #If Office is installed, modify the file name of the FFU
 #$Office = Get-childitem -Path 'M:\Program Files\Microsoft Office' -ErrorAction SilentlyContinue | Out-Null
 $Office = Get-childitem -Path 'M:\Program Files\Microsoft Office' -ErrorAction SilentlyContinue
-if($Office){
+if ($Office) {
     $ffuFilePath = "W:\$Name`_$DisplayVersion`_$SKU`_Office`_$BuildDate.ffu"
-    $dismArgs = "/capture-ffu /imagefile=$ffuFilePath /capturedrive=\\.\PhysicalDrive0 /name:$Name$DisplayVersion$SKU /Compress:Default"
-    
-    
-}
-else{
+} else {
     $ffuFilePath = "W:\$Name`_$DisplayVersion`_$SKU`_Apps`_$BuildDate.ffu"
-    $dismArgs = "/capture-ffu /imagefile=$ffuFilePath /capturedrive=\\.\PhysicalDrive0 /name:$Name$DisplayVersion$SKU /Compress:Default"
-    
+}
+$dismArgs = "/capture-ffu /imagefile=$ffuFilePath /capturedrive=\\.\PhysicalDrive0 /name:$Name$DisplayVersion$SKU /Compress:Default"
+
+if (![string]::IsNullOrEmpty($CustomFFUNameTemplate)) {
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{Name}", $Name
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{DisplayVersion}", $DisplayVersion
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{SKU}", $SKU
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{BuildDate}", $BuildDate
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{yyyy}", (Get-Date -UFormat "%Y")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{MM}", (Get-Date -UFormat "%m")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{dd}", (Get-Date -UFormat "%d")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{H}", (Get-Date -UFormat "%H")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{hh}", (Get-Date -UFormat "%I")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{mm}", (Get-Date -UFormat "%M")
+    $CustomFFUNameTemplate = $CustomFFUNameTemplate -replace "{tt}", (Get-Date -UFormat "%p")
+
+    if (!$CustomFFUNameTemplate.EndsWith(".ffu")) {
+        $CustomFFUNameTemplate += ".ffu"
+    }
+
+    $dismArgs = "/capture-ffu /imagefile=$CustomFFUNameTemplate /capturedrive=\\.\PhysicalDrive0 /name:$Name$DisplayVersion$SKU /Compress:Default"
 }
 
 #Unload Registry
