9cac674d7be4ae85a8555f5da70b3bc7610e8104
rbalsleyMSFT <richbal@microsoft.com>
2025-05-15 08:38:01 -0700

    Marged in changes to Add-WindowsPackage that were missing

--- FFUDevelopment/BuildFFUVM.ps1
+++ FFUDevelopment/BuildFFUVM.ps1
@@ -4626,7 +4626,7 @@ try {
             }
         }
         $CUPPath = "$KBPath\$CUPFileName"
-        WriteLog "Latest CU saved to $CUPPath"
+        WriteLog "Latest CU Preview saved to $CUPPath"
     }
 
     #Update Latest .NET Framework
@@ -4672,7 +4672,7 @@ try {
             }
         }
         $NETPath = "$KBPath\$NETFileName"
-        WriteLog "Latest CU saved to $NETPath"
+        WriteLog "Latest .NET Framework saved to $NETPath"
     }
 
     #Search for cached VHDX and skip VHDX creation if there's a cached version
@@ -4792,8 +4792,23 @@ try {
                     WriteLog 'SSU removed'
                     WriteLog "Adding CU to $WindowsPartition"
                 }
-                # Add-WindowsPackage -Path $WindowsPartition -PackagePath $KBPath -PreventPending | Out-Null
-                Add-WindowsPackage -Path $WindowsPartition -PackagePath $KBPath | Out-Null
+                
+                # Break out CU and NET updates to be added separately to abide by Checkpoint Update recommendations
+                if ($UpdateLatestCU) {
+                    WriteLog "Adding $CUPath to $WindowsPartition"
+                    Add-WindowsPackage -Path $WindowsPartition -PackagePath $CUPath | Out-Null
+                    WriteLog "$CUPath added to $WindowsPartition"
+                }
+                if ($UpdatePreviewCU) {
+                    WriteLog "Adding $CUPPath to $WindowsPartition"
+                    Add-WindowsPackage -Path $WindowsPartition -PackagePath $CUPPath | Out-Null
+                    WriteLog "$CUPPath added to $WindowsPartition"
+                }
+                if ($UpdateLatestNet) {
+                    WriteLog "Adding $NETPath to $WindowsPartition"
+                    Add-WindowsPackage -Path $WindowsPartition -PackagePath $NETPath | Out-Null
+                    WriteLog "$NETPath added to $WindowsPartition"
+                }
                 WriteLog "KBs added to $WindowsPartition"
                 if ($AllowVHDXCaching) {
                     $cachedVHDXInfo = [VhdxCacheItem]::new()
